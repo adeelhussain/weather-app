0651afa44bbb56b487337c2e00bed861d854d584
zunair zakir <zunair_zakir@yahoo.com>
2014-04-22 11:52:06 +0500

    hard cord structure

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function weather_app(){
+  var temp =   document.getElementById("temp").innerText;
+    console.log(Math.degrees(25));
+    var el = document.getElementById("body").style.backgroundImage;
+    if(temp >Math.degrees(25)) { // full value is provided.
+        console.log(temp)
+        el.style.backgroundImage = "url('img/ww07.JPG')"; // change it
+    }
+}
